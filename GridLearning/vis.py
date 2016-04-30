c2c9412e80720a76977475c2aeb2a4861d43a313
phiaseitz <sophia.seitz@students.olin.edu>
2016-04-30 15:15:28 -0400

    Better neural networ vis and new color map

--- GridLearning/vis.py
+++ GridLearning/vis.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+from mpl_toolkits.axes_grid1 import AxesGrid
 import numpy as np
 
 import tensorflow as tf
@@ -47,7 +48,7 @@ def visAllStatesGivenGoal (myRl, goalState):
 	for row,col in zip(possRow,possCol):
 		tempGameState = goalState.copy()
 		tempGameState[col, row] = 0
-		action = myRl.getAction(tempGameState.flatten()[np.newaxis,:], evaluation=True)
+		action = myRl.getAction(tempGameState.flatten()[np.newaxis,:], evaluation=True)[0]
 		
 		arrowTailX = col + 0.5
 		arrowTailY = row + 0.5
@@ -55,7 +56,7 @@ def visAllStatesGivenGoal (myRl, goalState):
 		arrowHeadX = 0.35*actionToChange[action][1]
 		arrowHeadY = 0.35*actionToChange[action][0]
 		ax.arrow(arrowTailX, arrowTailY, arrowHeadX, arrowHeadY, head_width=0.05, head_length=0.1, fc='k', ec='k')
-
+	ax.invert_yaxis()
 	plt.show()
 
 def visNN (nn, boardX, boardY):
@@ -66,26 +67,55 @@ def visNN (nn, boardX, boardY):
 	numNeurons = weights.shape[1]
 
 	# Set up the subplot stuff!
-	# WE're going to try to make a square
+	# We're going to try to make a square
 	subplotDim = int(round((float(numNeurons)**0.5)))
-	f, axarr = plt.subplots(subplotDim, subplotDim, figsize=(10, 10))
+	fig = plt.figure()
 
-	for neuronNum in range(numNeurons):
-		subplotRow = neuronNum/subplotDim
-		subplotCol = neuronNum % subplotDim
-		ax = axarr[subplotRow, subplotCol]
+	# f, axarr = plt.subplots(subplotDim, subplotDim, figsize=(10, 10))
+	grid = AxesGrid(fig, 111, 
+					nrows_ncols = (subplotDim, subplotDim),
+					axes_pad=0.05,
+					share_all=True,
+					label_mode="L",
+					cbar_location="right",
+					cbar_mode="single")
+	maxWeight = weights.max()
+	minWeight = weights.min()
+
+	for neuronNum,ax in enumerate(grid):
 		learnedWeights = weights[:,neuronNum]
 		weightsReshaped = learnedWeights.reshape((boardY, boardX))
 
-		mat = ax.matshow(weightsReshaped)
+		mat = ax.pcolor(weightsReshaped, 
+						vmin=minWeight, 
+						vmax=maxWeight, 
+						cmap="seismic")
 		ax.set_title('Neuron {0}'.format(neuronNum))
-		# plt.colorbar(mat)
-		# ax.colorbar()
-	f.tight_layout()
+		ax.invert_yaxis()
+	grid.cbar_axes[0].colorbar(mat)
+
+	# for cax in grid.cbar_axes:
+	# 	cax.toggle_label(False)
+
 	plt.show()
 
-	cax = f.add_axes([0.9, 0.1, 0.03, 0.8])
-	f.colorbar(mat, cax=cax)
+
+	# for neuronNum in range(numNeurons):
+	# 	subplotRow = neuronNum/subplotDim
+	# 	subplotCol = neuronNum % subplotDim
+	# 	ax = axarr[subplotRow, subplotCol]
+	# 	learnedWeights = weights[:,neuronNum]
+	# 	weightsReshaped = learnedWeights.reshape((boardY, boardX))
+
+	# 	mat = ax.pcolor(weightsReshaped, vmin=minWeight, vmax=maxWeight)
+	# 	ax.set_title('Neuron {0}'.format(neuronNum))
+	# 	# plt.colorbar(mat)
+	# 	# ax.colorbar()
+	# f.tight_layout()
+	# plt.show()
+
+	# cax = f.add_axes([0.9, 0.1, 0.03, 0.8])
+	# f.colorbar(mat, cax=cax)
 
 	#Now we need to slice this appropriately for each of the neurons. This returns
 
